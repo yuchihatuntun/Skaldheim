bc9a2afaf9d2068c555158d15e1bf0d414241d6d
Ruilin Xu <xurlin7@mail2.sysu.edu.cn>
2025-02-15 22:16:58 +0800

    name edit

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -137,7 +137,7 @@ export default defineConfig({
       { icon: 'discord', link: discordLink },
     ],
     footer: {
-      message: '用 <span style="color: #e25555;">&#9829;</span> 撰写',
+      message: 'Written with <span style="color: #e25555;">&#9829;</span>',
       copyright:
         '<a class="footer-cc-link" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> © 2022-PRESENT Nólëbase 的创作者们',
     },
