7459767f9d0959a6301cac1f09205eada7e922b7
Ruilin Xu <xurlin7@mail2.sysu.edu.cn>
2025-02-15 20:44:31 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = 'Skaldheim'
-export const siteShortName = 'Skaldheim'
-export const siteDescription = 'A place to record memories, knowledge and ideas'
+export const siteName = 'Nólëbase'
+export const siteShortName = 'Nólëbase'
+export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
