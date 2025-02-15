112f665106fc0356adcb8e6b34ba8bd31ff196ad
Ruilin Xu <xurlin7@mail2.sysu.edu.cn>
2025-02-15 20:08:52 +0800

    Modification: Creator information change

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -20,30 +20,17 @@ const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
+    name: 'Tuntun Yuchiha',
     avatar: '',
-    username: 'nekomeowww',
-    title: 'Nólëbase 原始创作者',
-    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
+    username: 'yuchihatuntun',
+    title: 'Knowledge base maintainer',
+    desc: 'Developer specializing in data analytics, embedded engineering, chip architecture design',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+      { type: 'github', icon: 'github', link: 'https://github.com/yuchihatuntun' },
+      { type: 'twitter', icon: 'twitter', link: 'https://x.com' },
     ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
-  },
-  {
-    name: '絢香音',
-    avatar: '',
-    username: 'LittleSound',
-    title: 'Nólëbase 原始创作者',
-    desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
-    ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
+    nameAliases: ['yuchihatuntun', 'Tuntun Yuchiha', 'Tuntun Yuchiha', 'Ruilin Xu', 'Xu Ruilin'],
+    emailAliases: ['xurlin7@mail2.sysu.edu.cn'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
